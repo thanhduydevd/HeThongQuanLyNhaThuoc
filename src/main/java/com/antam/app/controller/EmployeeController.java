1d432543992e6d0754f58b9fbd071cbe0f08dd32
DangKhoa902 <23653401.khoa@student.iuh.edu.vn>
2025-10-05 20:44:00 +0700

    update giao diện nhân viên

--- src/main/java/com/antam/app/controller/EmployeeController.java
+++ src/main/java/com/antam/app/controller/EmployeeController.java
@@ -5,13 +5,34 @@
 
 package com.antam.app.controller;
 
+import com.antam.app.dao.NhanVien_DAO;
+import com.antam.app.entity.NhanVien;
 import com.antam.app.gui.ShowDialog;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class EmployeeController {
     @FXML
     private Button btnAddEmployee;
+
+    @FXML
+    private TableView<NhanVien> tbNhanVien;
+    @FXML
+    private Button btnFindNV;
+    @FXML
+    private TextField textFindNV;
+    @FXML
+    private TableColumn<NhanVien, String> colMaNV, colHoTen, colChucVu, colSDT, colDiaChi,colEmail;
+    @FXML
+    private TableColumn<NhanVien, Double> colLuong;
+
+    ArrayList<NhanVien> list = NhanVien_DAO.getDsNhanVienformDBS();
 
     public EmployeeController() {
     }
@@ -20,5 +41,12 @@ public class EmployeeController {
         this.btnAddEmployee.setOnAction((e) -> {
             (new ShowDialog("themnhanvien")).showAndWait();
         });
+        this.btnFindNV.setOnAction((e)->{
+
+        });
+    }
+
+    public void loadNhanVien(){
+//        FXMLLoader load = FXMLLoader.get
     }
 }
--- src/main/java/com/antam/app/dao/PhieuDat_DAO.java
+++ src/main/java/com/antam/app/dao/PhieuDat_DAO.java
@@ -6,19 +6,21 @@ package com.antam.app.dao;/*
  */
 
 import com.antam.app.connect.ConnectDB;
-import com.antam.app.entity.NhanVien;
 import com.antam.app.entity.PhieuDatThuoc;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class PhieuDat_DAO {
-    public static ArrayList<PhieuDatThuoc> list ;
+    public static ArrayList<PhieuDatThuoc> list = PhieuDat_DAO.getAllPhieuDatThuocFromDBS();
 
+    /**
+     * Lấy toàn bộ danh sách phiếu đặt thuốc với thông tin bao gồm
+     * mã, ngày tạo, đã thanh toán hay chưa, mã khách, mã nhân viên tạo
+     * thành tiền của phiếu đặt
+     * @return Array[PhieuDatThuoc]
+     */
     public static ArrayList<PhieuDatThuoc> getAllPhieuDatThuocFromDBS(){
         ArrayList<PhieuDatThuoc> ds = new ArrayList<>();
         try {
@@ -29,7 +31,7 @@ public class PhieuDat_DAO {
         Connection con = ConnectDB.getConnection();
 
         try {
-            String sql = "select * form PhieuDatThuoc";
+            String sql = "select * from PhieuDatThuoc";
             PreparedStatement state = con.prepareStatement(sql);
             ResultSet kq = state.executeQuery();
             while(kq.next()){
@@ -51,4 +53,69 @@ public class PhieuDat_DAO {
         }
         return ds;
     }
+
+    public static boolean themPhieuDatThuocVaoDBS(PhieuDatThuoc i){
+        try {
+            ConnectDB.getInstance().connect();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Connection con = ConnectDB.getConnection();
+        // kiểm tra đã tồn tại hay chưa
+        try {
+            String sql = "select * from PhieuDatThuoc where MaPDT = ?";
+            PreparedStatement state = con.prepareStatement(sql);
+            state.setString(1,i.getMaPhieu());
+            ResultSet check = state.executeQuery();
+            if (check.next()){
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String updateSQL = "insert from PhieuDatThuoc values , [MaPDT] = ?," +
+                    " [NgayTao] = ?,[IsThanhToan] = ?,[MaKH] = ?, [MaNV] = ?, [MaKM] = ?," +
+                    "[TongTien] = ?";
+            PreparedStatement state = con.prepareStatement(updateSQL);
+            state.setString(1,i.getMaPhieu());
+            state.setDate(2, Date.valueOf(i.getNgayTao()));
+            state.setBoolean(3,i.isThanhToan());
+            state.setString(4,i.getKhachHang().getMaKH());
+            state.setString(5,i.getNhanVien().getMaNV());
+            state.setString(6,i.getKhuyenMai().getMaKM());
+            state.setDouble(7,i.getTongTien());
+            int kq = state.executeUpdate();
+            return kq >0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Xoá phiếu đặt thuốc trong DBS bằng cách tắt trạng thái hoạt động.
+     * @param maPDT mã phiếu đặt thuốc
+     * @return true nếu cập nhật thành công. false nếu không thể cập nhật.
+     */
+    public static boolean xoaPhieuDatThuocTrongDBS(String maPDT){
+        try {
+            ConnectDB.getInstance().connect();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Connection con = ConnectDB.getConnection();
+
+        try {
+            String sql = "update PhieuDatThuoc set DeleteAt = 1 where MaPDT = ?";
+            PreparedStatement state = con.prepareStatement(sql);
+            state.setString(1,maPDT);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+
 }
